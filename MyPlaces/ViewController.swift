0e766d2c74e784b2ffe210e268e0831fe2b3f881
Edgar Hakobyan <ehakobyan94@gmail.com>
2021-06-29 00:02:10 +0400

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Edgar on 29.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
